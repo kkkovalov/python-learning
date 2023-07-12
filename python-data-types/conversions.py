d6bb3f4ba304a11d2c9cf37407a586d3638e93c5
kkkovalov <vlad_kovalov@icloud.com>
2023-07-11 18:47:24 -0700

    Data type conversions, int to Float

---
+++ python-data-types/conversions.py
@@ -0,0 +1,17 @@
+integerNumber = 124
+floatNumber = 1.546
+
+stringWord = 'cool'
+stringSentence = 'blue t-shirt'
+
+
+print(integerNumber + floatNumber)
+print(integerNumber * floatNumber)
+print(type(integerNumber))
+print(type(floatNumber))
+
+print("Integer to Float")
+print(float(integerNumber),type(integerNumber))
+
+print("Float to Integer")
+print(int(floatNumber), type(floatNumber))
