6a673402c06a9a7d374f1819606d72f96a5b75ea
kkkovalov <vlad_kovalov@icloud.com>
2023-07-11 19:58:07 -0700

    Lists, tuples, sets and dictionaries

---
+++ python-data-types/list-tuple-set-dict.py
@@ -0,0 +1,34 @@
+#Lists
+
+a = [1,2,3,4,5,6,1,2,3,4]
+
+a.insert(2,54)
+a.pop()
+
+print(a)
+
+#Set
+
+s = set()
+
+s.add(4)
+s.add(3)
+s.add(4)
+s.add(1)
+s.pop()
+print(s)
+
+#Tuple
+# Immutable
+t = (5,6,1,4,8,5,2)
+
+print(t)
+
+#Dictionary
+d = {}
+
+d[0] = "Three"
+d[2] = 5
+d[4] = 'nice'
+
+print(d)
