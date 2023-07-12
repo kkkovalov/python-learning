b2521dfbf30718b1dbcdfa30d1e623a020bae542
kkkovalov <vlad_kovalov@icloud.com>
2023-07-11 18:58:10 -0700

    Functions.py for simple function creation

---
+++ python-data-types/functions.py
@@ -0,0 +1,16 @@
+#Simple function
+def sum(a,b):
+    return a+b
+
+print(sum(1,5))
+
+#lambda function
+def f():
+    return "Good luck, buddy."
+
+print(f())
+
+def nicePass():
+    pass
+
+print(nicePass())
