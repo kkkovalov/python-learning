55687de285479718d4ed545915459016c8890d06
kkkovalov <vlad_kovalov@icloud.com>
2023-07-12 20:41:34 -0700

    Finished queues.py full functonality

--- datastructures/queues.py
+++ datastructures/queues.py
@@ -12,9 +12,11 @@ class Queue:
             c_queue = QueueObject(data=queues.pop(0))
             self.head = c_queue
             self.tail = c_queue
-        for item in queues:
-            c_queue.next = QueueObject(data=item)
-            c_queue = c_queue.next
+            for item in queues:
+                c_queue.next = QueueObject(data=item)
+                c_queue = c_queue.next
+            self.tail = c_queue
+        
         
     
     def __repr__(self):
@@ -31,13 +33,35 @@ class Queue:
         while first_in_queue is not None:
             yield first_in_queue.data
             first_in_queue = first_in_queue.next
-            
-    def 
+    
+    def enqueue(self, data):
+        new_item = QueueObject(data=data)
+        self.tail.next = new_item
+        return None
+    
+    def dequeue(self):
+        removed_item = self.head.data
+        self.head = self.head.next
+        return removed_item    
+        
+    def front(self):
+        return self.head.data
+    
+    def last(self):
+        return self.tail.data
+        
             
     
 
 
 qqueue = Queue([1,4,9,23,43,8,3])
 
-for item in qqueue:
-    print(item)
+print(qqueue)
+
+print(qqueue.front(), qqueue.last())
+
+qqueue.enqueue(431)
+qqueue.dequeue()
+qqueue.dequeue()
+
+print(qqueue)
