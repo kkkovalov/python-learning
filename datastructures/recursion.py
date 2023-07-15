1294a48e3525aac0a33258b246eb8385692f7196
kkkovalov <vlad_kovalov@icloud.com>
2023-07-14 19:37:20 -0700

    Recursion fibonacci

--- datastructures/recursion.py
+++ datastructures/recursion.py
@@ -5,7 +5,21 @@ def factorial(n):
         return n*factorial(n-1)
     
 def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n, 1, 1, 2)
     
-    
-    
-print(factorial(1))
+def fib(n, prev, cur, count):
+    if n == count:
+        return cur
+    else:
+        temp = cur
+        cur += prev
+        prev = temp
+        count+=1
+        return fib(n,prev, cur, count)
+
+print(fibonacci(16))
