239421bddcdb5ca706f66bfbe1a26606add42f0a
kkkovalov <vlad_kovalov@icloud.com>
2023-07-15 12:51:52 -0700

    Decorators in Python

--- adv-topics/decorators.py
+++ adv-topics/decorators.py
@@ -1,12 +1,60 @@
-def shout(text):
-    return text.upper()
+def uppercase_decorator(function):
+    def wrapper():
+        func = function()
+        make_upper = func.upper()
+        return make_upper
+    return wrapper
 
-def whisper(text):
-    return text.lower()
+@uppercase_decorator
+def say_hi():
+    """
+    Function returns 'Hi there!' message in addition to its decorators
+    """
+    return 'Hi there!'
 
-def greet(func):
-    greeting = func('Hello, I am new here! YAY')
-    print(greeting)
+print(say_hi())
+
+
+import functools
+# Universal decorators
+def a_passing_argument(function_to_decorate):
+    @functools.wraps(function_to_decorate)
+    def wrapper(*args, **kwargs):
+        print('The positional arguments are', args)
+        print('The keyword arguments are', kwargs)
+        function_to_decorate(*args)
+    return wrapper
+
+@a_passing_argument
+def function_no_arg():
+    print("Inside func, no args")
     
     
-greet(shout)
+function_no_arg()
+
+import time
+
+def timer(function_to_decorate):
+    @functools.wraps(function_to_decorate)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = function_to_decorate(*args, **kwargs)
+        stop_time = time.time()
+        dt = stop_time - start_time
+        print(f"∆t = {dt}")
+        return result
+    return wrapper
+
+def fibonacci(n):
+    if not isinstance(n, int) or n < 1:
+        raise ValueError(f"{n} is not a positive integer")
+    
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+@timer
+def global_fib(n):
+    return fibonacci(n)
+    
+print(global_fib(8))
