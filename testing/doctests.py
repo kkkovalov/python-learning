35f4b8e8a47598d244e081f54a038aefd4732737
kkkovalov <vlad_kovalov@icloud.com>
2023-07-15 19:58:34 -0700

    Doctests in Python

---
+++ testing/doctests.py
@@ -0,0 +1,24 @@
+def count_vowels(word: str):
+    """
+    Given a single word, returns a total count of vowels in that word
+    
+    :param word: str
+    :return: int
+    
+    >>> count_vowels('Camila')
+    3
+    >>> count_vowels('Llrt')
+    0
+    >>> count_vowels('Aora')
+    3
+    
+    """
+    from re import search as regex
+    total_count = len([x for x in str(word) if regex("[aAeEuUiIoO]", x)])
+    return total_count
+
+print(count_vowels('aeyuio'))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
